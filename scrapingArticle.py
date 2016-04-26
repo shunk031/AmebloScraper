630555dbc5a74013d5c9a5a93a883c70c4d109bd
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-04-26 10:23:59 +0900

    add and update some functions

--- scrapingArticle.py
+++ scrapingArticle.py
@@ -3,6 +3,7 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+import time
 
 
 def scrapingArticleText(url):
@@ -10,6 +11,7 @@ def scrapingArticleText(url):
     引数から得たURLからブログ本文を取得して
     一文ずつ区切ったstringのlistをreturnする
     """
+
     try:
         html = urlopen(url)
     except HTTPError as e:
@@ -17,18 +19,11 @@ def scrapingArticleText(url):
         return None
     try:
         soup = BeautifulSoup(html.read(), "lxml")
-        lawArticleText = ""
 
         date = soup.find('time').string
+        lawArticleText = soup.find("div", {"class": "articleText"})
 
-        for i in soup.findAll("", {"class": "articleText"}):
-            lawArticleText += i.get_text()
-
-        articleText = lawArticleText.replace(u"\xa0", "\n")
-        articleText = articleText.split("\n")
-
-        while articleText.count("") > 0:
-            articleText.remove("")
+        articleText = cleanArticleText(lawArticleText)
 
     except AttributeError as e:
         return None
@@ -36,19 +31,52 @@ def scrapingArticleText(url):
     return dict(date=date, article=articleText)
 
 
+def cleanArticleText(lawArticle):
+
+    cleanText = "\n".join(lawArticle.strings)
+    cleanText = cleanText.replace(u"\xa0", "\n")
+    cleanText = cleanText.split("\n")
+
+    while cleanText.count("") > 0:
+        cleanText.remove("")
+
+    return cleanText
+
+
+def crawler(url):
+    """
+    次の記事のリンクを再帰的に所得する
+    """
+
+    html = urlopen(url)
+    soup = BeautifulSoup(html, "lxml")
+
+    nextPage = soup.find("a", {"class": "pagingNext"})
+
+    if nextPage is not None:
+
+        if 'href' in nextPage.attrs:
+            print(nextPage.attrs['href'])
+
+        time.sleep(5)
+        result = scrapingArticleText(nextPage.attrs['href'])
+
+        if result == None:
+            print("Can't find the text")
+
+        else:
+            print(result['date'])
+
+            for i in result['article']:
+                print(i)
+
+            crawler(nextPage.attrs['href'])
+
+
 def main():
-    url = "http://ameblo.jp/ogurayui-0815/entry-12145717070.html"
-    result = scrapingArticleText(url)
 
-    if result == None:
-        print("Can't find the text")
-
-    else:
-        print(result['date'])
-
-        for i in result['article']:
-            print(i)
-
+    crawler("http://ameblo.jp/ogurayui-0815/")
+    print("Finish!")
 
 if __name__ == '__main__':
     main()
