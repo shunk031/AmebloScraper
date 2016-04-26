1644a6f37473fc7a260ca0f701169f191401d2c2
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-04-26 10:26:42 +0900

    rename some files

--- draft_script.py
+++ draft_script.py
@@ -2,11 +2,16 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import time
 
 html = urlopen("http://ameblo.jp/ogurayui-0815/")
 soup = BeautifulSoup(html, "lxml")
 
-nextPage = soup.find("a", {"class": "pagingNext"})
+nextPage = soup.find("a", {"class": "apagingNext"})
+
+print(nextPage)
+
+print(nextPage is not None)
 
 if 'href' in nextPage.attrs:
     print(nextPage.attrs['href'])
