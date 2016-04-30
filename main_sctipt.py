64e1738f9efbacf5ef251efeceaa974099063ac5
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-04-30 11:58:40 +0900

    create AmebloCrawler class
    move some functions to AmebloCrawler class

--- main_sctipt.py
+++ main_sctipt.py
@@ -9,121 +9,128 @@ import datetime
 import sys
 
 
-def scrapingArticleText(url):
+class AmebloCrawler:
     """
-    引数から得たURLからブログ本文を取得して
-    日付、ブログタイトル、ブログ本文を
-    一文ずつ区切ったstringのlistとディクショナリとして
-    returnする
+    アメブロをクロールするためのクラス定義
     """
 
-    try:
+    def __init__(self, targetUrl):
+        self.targetUrl = targetUrl
+        self.beforeUrl = targetUrl
+        self.afterUrl = targetUrl
+
+    def scrapingArticleText(self, url):
+        """
+        引数から得たURLからブログ本文を取得して
+        日付、ブログタイトル、ブログ本文を
+        一文ずつ区切ったstringのlistとディクショナリとして
+        returnする
+        """
+
+        try:
+            html = urlopen(url)
+        except HTTPError as e:
+            print(e)
+            return None
+        try:
+            soup = BeautifulSoup(html.read(), "lxml")
+
+            # 日付情報の処理
+            date = soup.find('time').string
+            d = datetime.datetime.strptime(date, '%Y年%m月%d日')
+            date_string = d.strftime('%Y-%m-%d')
+
+            # ブログ記事タイトルの取得
+            titleText = soup.find(class_="skinArticleTitle").string
+
+            # ブログ記事の取得
+            lawArticleTexts = soup.find("div", {"class": "articleText"})
+
+            # ブログ記事データをクリーニング
+            articleTexts = self.cleanArticleTexts(lawArticleTexts)
+
+        except AttributeError as e:
+            return None
+
+        # 投稿日時と記事内容を格納する辞書データをリターンする
+        return dict(date=date_string, title=titleText, article=articleTexts)
+
+    def cleanArticleTexts(self, lawArticle):
+        """
+        記事データをクリーニングする
+        """
+        # 取得したHTMLファイル中のbrタグを改行文字に変更
+        cleanTexts = "\n".join(lawArticle.strings)
+        # 不要な文字列を改行文字に変更
+        cleanTexts = cleanTexts.replace(u"\xa0", "\n")
+        # 改行文字で文字列を区切る
+        cleanTexts = cleanTexts.split("\n")
+
+        # リスト内に要素が存在しない場合は取り除く
+        while cleanTexts.count("") > 0:
+            cleanTexts.remove("")
+
+        return cleanTexts
+
+    def getNextPageLink(self, url):
+        """
+        ブログ本文から次のページへのリンクを取得する
+        リンクが存在しない場合はNoneがreturnする
+        """
+
         html = urlopen(url)
-    except HTTPError as e:
-        print(e)
-        return None
-    try:
         soup = BeautifulSoup(html.read(), "lxml")
 
-        # 日付情報の処理
-        date = soup.find('time').string
-        d = datetime.datetime.strptime(date, '%Y年%m月%d日')
-        date_string = d.strftime('%Y-%m-%d')
+        nextPage = soup.find("a", {"class": "pagingNext"})
 
-        # ブログ記事タイトルの取得
-        titleText = soup.find(class_="skinArticleTitle").string
+        if nextPage is not None:
 
-        # ブログ記事の取得
-        lawArticleTexts = soup.find("div", {"class": "articleText"})
+            if 'href' in nextPage.attrs:
+                nextPageLink = nextPage.attrs['href']
+                print("\n" + nextPageLink)
 
-        # ブログ記事データをクリーニング
-        articleTexts = cleanArticleTexts(lawArticleTexts)
+                return nextPageLink
+        else:
+            return None
 
-    except AttributeError as e:
-        return None
+    def saveArticleTexts(self, articleTexts, filename):
+        """
+        date, articleTextを保持する辞書を受け取り、
+        CSVファイルにアウトプットする
+        ブログ毎に出力ファイル名をファイル名を変更する
+        """
 
-    # 投稿日時と記事内容を格納する辞書データをリターンする
-    return dict(date=date_string, title=titleText, article=articleTexts)
+        csvFile = open(filename, 'a', newline='', encoding='utf-8')
 
+        try:
+            writer = csv.writer(csvFile)
+            # writer.writerow(('date', 'article'))
 
-def cleanArticleTexts(lawArticle):
-    """
-    記事データをクリーニングする
-    """
-    # 取得したHTMLファイル中のbrタグを改行文字に変更
-    cleanTexts = "\n".join(lawArticle.strings)
-    # 不要な文字列を改行文字に変更
-    cleanTexts = cleanTexts.replace(u"\xa0", "\n")
-    # 改行文字で文字列を区切る
-    cleanTexts = cleanTexts.split("\n")
+            for articleText in articleTexts['article']:
+                writer.writerow(
+                    (articleTexts['date'], articleTexts['title'], articleText))
 
-    # リスト内に要素が存在しない場合は取り除く
-    while cleanTexts.count("") > 0:
-        cleanTexts.remove("")
+        finally:
+            csvFile.close()
 
-    return cleanTexts
+    def crawler(self, url, filename):
+        """
+        次の記事のリンクを再帰的に所得する
+        """
+        results = self.scrapingArticleText(url)
 
+        if results is None:
+            print("Can't find the text")
 
-def getNextPageLink(url):
-    """
-    ブログ本文から次のページへのリンクを取得する
-    リンクが存在しない場合はNoneがreturnする
-    """
+        else:
+            print(results['date'])
+            print(results['title'])
+            for result in results['article']:
+                print(result)
 
-    html = urlopen(url)
-    soup = BeautifulSoup(html.read(), "lxml")
+            self.saveArticleTexts(results, filename)
 
-    nextPage = soup.find("a", {"class": "pagingNext"})
-
-    if nextPage is not None:
-
-        if 'href' in nextPage.attrs:
-            nextPageLink = nextPage.attrs['href']
-            print("\n" + nextPageLink)
-
-            return nextPageLink
-    else:
-        return None
-
-
-def saveArticleTexts(articleTexts, filename):
-    """
-    date, articleTextを保持する辞書を受け取り、
-    CSVファイルにアウトプットする
-    ブログ毎に出力ファイル名をファイル名を変更する
-    """
-    csvFile = open(filename, 'a', newline='', encoding='utf-8')
-
-    try:
-        writer = csv.writer(csvFile)
-        # writer.writerow(('date', 'article'))
-
-        for articleText in articleTexts['article']:
-            writer.writerow(
-                (articleTexts['date'], articleTexts['title'], articleText))
-
-    finally:
-        csvFile.close()
-
-
-def crawler(url, filename):
-    """
-    次の記事のリンクを再帰的に所得する
-    """
-    results = scrapingArticleText(url)
-
-    if results is None:
-        print("Can't find the text")
-
-    else:
-        print(results['date'])
-        print(results['title'])
-        for result in results['article']:
-            print(result)
-
-        saveArticleTexts(results, filename)
-
-        time.sleep(5)
+            time.sleep(5)
 
 
 def main():
@@ -132,20 +139,22 @@ def main():
     argc = len(argvs)
 
     if argc == 2:
-        target_url = argvs[1]
+        targetUrl = argvs[1]
     else:
         # デフォルトのターゲットURLを利用する
-        target_url = "http://ameblo.jp/ogurayui-0815/"
+        targetUrl = "http://ameblo.jp/ogurayui-0815/"
 
-    filename = target_url.replace("http://ameblo.jp/", "")
+    ac = AmebloCrawler(targetUrl)
+
+    filename = targetUrl.replace("http://ameblo.jp/", "")
     filename = filename.rstrip("/")
     filename = filename + ".csv"
 
     while True:
-        crawler(target_url, filename)
-        target_url = getNextPageLink(target_url)
+        ac.crawler(targetUrl, filename)
+        targetUrl = ac.getNextPageLink(targetUrl)
 
-        if target_url is None:
+        if targetUrl is None:
             break
 
     print("Finish!")
